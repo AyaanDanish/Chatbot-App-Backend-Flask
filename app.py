4ee053af22c2219e80979970bb2b47cd52107395
AyaanDanish <ayaandanish7@gmail.com>
2023-11-21 19:35:15 +0500

    Restored to original version

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
-import requests
+import tiktoken
 from dotenv import load_dotenv
 import os
 
@@ -10,69 +10,61 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# Set up Azure OpenAI configuration
 openai.api_type = "azure"
 openai.api_version = "2023-08-01-preview"
 openai.api_base = os.environ.get("API_BASE")
 openai.api_key = os.environ.get("API_KEY")
 deployment_id = os.environ.get("DEPLOYMENT_ID")
 
-search_endpoint = os.environ.get("SEARCH_ENDPOINT")
-search_key = os.environ.get("SEARCH_KEY")
-search_index_name = os.environ.get("SEARCH_INDEX_NAME")
+max_response_tokens = 250
+token_limit = 4000
 
 
-def setup_byod(deployment_id: str) -> None:
-    """Sets up the OpenAI Python SDK to use your own data for the chat endpoint.
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
+    encoding = tiktoken.encoding_for_model(model)
+    num_tokens = 0
+    for message in messages:
+        num_tokens += (
+            4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+        )
 
-    :param deployment_id: The deployment ID for the model to use with your own data.
-
-    To remove this configuration, simply set openai.requestssession to None.
-    """
-
-    class BringYourOwnDataAdapter(requests.adapters.HTTPAdapter):
-        def send(self, request, **kwargs):
-            request.url = f"{openai.api_base}/openai/deployments/{deployment_id}/extensions/chat/completions?api-version={openai.api_version}"
-            return super().send(request, **kwargs)
-
-    session = requests.Session()
-
-    session.mount(
-        prefix=f"{openai.api_base}/openai/deployments/{deployment_id}",
-        adapter=BringYourOwnDataAdapter(),
-    )
-
-    openai.requestssession = session
+        for key, value in message.items():
+            if value != None:
+                num_tokens += len(encoding.encode(value))
+            if key == "name":  # if there's a name, the role is omitted
+                num_tokens += -1  # role is always required and always 1 token
+    num_tokens += 2  # every reply is primed with <im_start>assistant
+    return num_tokens
 
 
+# Sends a message to the backend
 @app.route("/send_to_backend", methods=["POST"])
-def send_to_backend():
+def chat():
     data = request.json
     user_input = data.get("userMsg")
-
-    # Build conversation
-    conversation = [{"role": "user", "content": user_input}]
-
-    setup_byod(deployment_id)
-
-    completion = openai.ChatCompletion.create(
-        messages=conversation,
-        deployment_id=deployment_id,
-        dataSources=[
-            {
-                "type": "AzureCognitiveSearch",
-                "parameters": {
-                    "endpoint": search_endpoint,
-                    "key": search_key,
-                    "indexName": search_index_name,
-                },
-            }
-        ],
+    conversation = data.get(
+        "conversation", [{"role": "system", "content": "You are a helpful assistant."}]
     )
 
-    response_content = completion["choices"][0]["message"]["content"]
+    conversation.append({"role": "user", "content": user_input})
 
-    return jsonify({"response": response_content})
+    num_tokens = num_tokens_from_messages(conversation)
+    while num_tokens + max_response_tokens >= token_limit:
+        del conversation[1]
+        num_tokens = num_tokens_from_messages(conversation)
+
+    response = openai.ChatCompletion.create(
+        engine="myassist-model",
+        messages=conversation,
+        temperature=0.7,
+        max_tokens=token_limit,
+    )
+
+    conversation.append(
+        {"role": "assistant", "content": response["choices"][0]["message"]["content"]}
+    )
+
+    return jsonify(conversation)
 
 
 if __name__ == "__main__":
