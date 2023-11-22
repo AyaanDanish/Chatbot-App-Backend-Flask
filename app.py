b8dfad0b6b59868abf6ed7123a9b7e272e6af6ef
AyaanDanish <ayaandanish7@gmail.com>
2023-11-22 20:03:23 +0500

    Added a home page for testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import openai
 import tiktoken
@@ -20,20 +20,23 @@ max_response_tokens = 250
 token_limit = 4000
 
 
+@app.route("/", methods=["GET"])
+def home_page():
+    return render_template("index.html")
+
+
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
     encoding = tiktoken.encoding_for_model(model)
     num_tokens = 0
     for message in messages:
-        num_tokens += (
-            4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-        )
+        num_tokens += 4
 
         for key, value in message.items():
             if value != None:
                 num_tokens += len(encoding.encode(value))
-            if key == "name":  # if there's a name, the role is omitted
-                num_tokens += -1  # role is always required and always 1 token
-    num_tokens += 2  # every reply is primed with <im_start>assistant
+            if key == "name":
+                num_tokens += -1
+    num_tokens += 2
     return num_tokens
 
 
@@ -69,5 +72,3 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
