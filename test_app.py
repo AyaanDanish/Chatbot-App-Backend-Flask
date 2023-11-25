cc214171364071c6728459afff4e2f01e593483d
AyaanDanish <ayaandanish7@gmail.com>
2023-11-25 21:56:45 +0500

    Cleaned up test app

--- test_app.py
+++ test_app.py
@@ -1,35 +1,31 @@
 import pytest
-from flask import Flask
 from app import app, chat
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
+
 def test_home_page(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
 
+
 def test_favicon(client):
-    response = client.get('/favicon.ico')
+    response = client.get("/favicon.ico")
     assert response.status_code == 200
-    assert response.mimetype == 'image/vnd.microsoft.icon'
+    assert response.mimetype == "image/vnd.microsoft.icon"
+
 
 def test_chat(client):
     data = {
         "userMsg": "Hello",
-        "conversation": [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant."
-            }
-        ]
+        "conversation": [{"role": "system", "content": "You are a helpful assistant."}],
     }
-    response = client.post('/send_to_backend', json=data)
+    response = client.post("/send_to_backend", json=data)
     assert response.status_code == 200
